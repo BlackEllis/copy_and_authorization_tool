fe812108df096589c0cf321acbf4809c273e424a
TORI <chronoborder@gmail.com>
2017-03-15 16:45:14 +0900

    [add] verification_toolの基本骨子まで
    + log設定まで実装

--- source/verification_tool/Program.cs
+++ source/verification_tool/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using tool_commons.modules;
 
 namespace verification_tool
 {
@@ -10,6 +11,61 @@ namespace verification_tool
     {
         static void Main(string[] args)
         {
+            // 起動引数から各パラメータに切り分けた連想配列を生成
+            Dictionary<string, string> hash_array = utility_tools.argument_decomposition(args);
+
+            try
+            {
+                Console.WriteLine("read setting from json : start");
+                json_module.setup(
+                    utility_tools.get_value_from_hasharray(hash_array,
+                                                            constant.RESOURCES_KEY_EXTERNAL,
+                                                            constant.RESOURCES_DIR + constant.EXTERNAL_RESOURCE_FILENAME)
+                );
+                Console.WriteLine("read setting from json : end");
+
+                // 各外部ファイルのディレクトリ先を設定
+                string log_dir = json_module.get_external_resource("log_file_dir", constant.LOG_FILE_DIR);
+                string export_dir = json_module.get_external_resource("export_dir", constant.EXPORT_DIR);
+                string resources_dir = utility_tools.get_value_from_hasharray(hash_array, "RESOURCES_DIR", constant.RESOURCES_DIR);
+
+                Console.WriteLine("log feature setup : start");
+                setup_logs(hash_array, log_dir); // ログ、エラーファイルのセットアップ
+                Console.WriteLine("log feature setup : end");
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.WriteLine("press any key to exit.");
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// ログ、エラーファイルのセットアップ関数
+        /// </summary>
+        /// <param name="args">起動引数を連想配列にしたもの</param>
+        /// <param name="log_dir"></param>
+        private static void setup_logs(Dictionary<string, string> args, string log_dir)
+        {
+            // ログファイルの関係設定
+            string log_file = json_module.get_external_resource("default_log_filename", constant.DEFAULT_LOG_FILENAME);
+            log_file = utility_tools.get_value_from_hasharray(args, constant.RESOURCES_KEY_LOG, log_file);
+            string log_encode = json_module.get_external_resource("log_file_encode", constant.LOG_FILE_ENCODE);
+
+            // 抽出ログファイルの関係設定
+            string extracting_file = json_module.get_external_resource("default_extracting_filename", constant.DEFAULT_EXTRACTING_FILENAME);
+            extracting_file = utility_tools.get_value_from_hasharray(args, constant.RESOURCES_KEY_EXTRACTINGLOG, extracting_file);
+            string extracting_encode = json_module.get_external_resource("extracting_file_encode", constant.EXTRACTING_FILE_ENCODE);
+
+#if DEBUG
+            loger_module.loger_setup(log_file, log_dir, log_encode, "info", true);
+            loger_module.loger_setup(extracting_file, log_dir, extracting_encode, "extracting", true);
+#else
+            loger_module.loger_setup(log_file, log_dir, log_encode, "info");
+            loger_module.loger_setup(extracting_file, log_dir, extracting_encode, "extracting");
+#endif
         }
     }
 }
---
+++ source/verification_tool/constant.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace verification_tool
+{
+    class constant
+    {
+        // 引数でのキーワード
+        public const string RESOURCES_KEY_EXTERNAL = "EXTERNAL_FILE";           // 外部設定ファイル参照フォルダ設定オプション
+        public const string RESOURCES_KEY_LOG = "LOG_FILE";                     // システムログ出力先設定オプション
+        public const string RESOURCES_KEY_EXTRACTINGLOG = "EXTRACTING_FILE";    // 抽出ログ出力先設定オプション
+
+        // デフォルトの外部設定ファイルの情報
+        public const string EXTERNAL_RESOURCE_ENCODE = "utf-8";                     // 文字コード
+        public const string EXTERNAL_RESOURCE_FILENAME = "external_setting.json";   // 設定ファイル名
+
+        // log ファイル関係定数
+        public const string LOG_FILE_ENCODE = "utf-8";                      // 出力ログファイルのデフォルト文字コード
+        public const string DEFAULT_LOG_FILENAME = "system.log";            // 出力ログファイルのデフォルト名
+
+        // extracting ファイル関係定数
+        public const string EXTRACTING_FILE_ENCODE = "utf-8";                // 出力抽出ログファイルのデフォルト文字コード
+        public const string DEFAULT_EXTRACTING_FILENAME = "errors.log";      // 出力抽出ログファイルのデフォルト名
+
+        // 固定ディレクトリ設定
+        public const string RESOURCES_DIR = "./resources/";
+        public const string LOG_FILE_DIR = "./log/";
+        public const string EXPORT_DIR = "./export/";
+    }
+}
